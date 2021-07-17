7f759dc6d4e94c1d16b24dfff322462112eb82c8
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-07-17 13:26:45 -0500

    Retornando número totla de registros en una colección

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -11,12 +11,26 @@ const usuariosGet = async (req = request, res = response) => {
     // const { q, nombre, apikey } = req.query;
 
     const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
 
-    const usuarios = await Usuario.find()
-        .skip(Number(desde))
-        .limit(Number(limite));
+    /*     const usuarios = await Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite));
 
-    res.json({ usuarios });
+        const total = await Usuario.countDocuments(query); */
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ])
+
+    res.json({
+        /* resp */
+        total,
+        usuarios
+    });
 }
 
 const usuariosPost = async (req, res = response) => {
