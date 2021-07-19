bc01ca71b4ca90586265e959328f1d104243e7a5
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-07-18 19:51:57 -0500

    Configurando el login de usuario

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,56 @@
+const bcryptjs = require('bcryptjs');
+const { response } = require('express');
+const Usuario = require('../models/usuario')
+
+
+const login = async (req, res = response) => {
+
+    const { correo, password } = req.body;
+
+    try {
+
+        //Verificar si el correo existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario/password no son correctos - correo'
+            })
+
+        }
+
+        //Verificar si el usuario esta activo en la base de datos
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario/password no son correctos - estado: false'
+            })
+
+        }
+        //Verificar la contraseña
+        const isValidPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!isValidPassword) {
+            return res.status(400).json({
+                msg: 'Usuario/password no son correctos - password'
+            })
+
+        }
+
+
+        //Generar JWT
+
+
+        res.json({
+            msg: 'Login ok'
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable cn el administrador'
+        })
+    }
 
 
 
-const login = (req, res = response) => {
-
-    res.json({
-        msg: 'Login ok'
-    })
 }
 
 
