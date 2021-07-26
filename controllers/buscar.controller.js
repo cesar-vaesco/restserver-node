db526363b4b9400a4c71a95ae126d58ef15001aa
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-07-26 08:17:42 -0500

    Búsqueda de usario por id

--- controllers/buscar.controller.js
+++ controllers/buscar.controller.js
@@ -1,4 +1,27 @@
 const { response } = require("express");
+const { ObjectId } = require("mongoose").Types;
+
+
+const { Usuario, Categoria, Producto } = require('../models')
+
+const coleccionesPermitidas = [
+    'categoria',
+    'productos',
+    'roles',
+    'usuarios',
+];
+
+const buscarUsuarios = async (termino = '', res = response) => {
+
+    const esMongoID = ObjectId.isValid(termino);
+
+    if (esMongoID) {
+        const usuario = await Usuario.findById(termino);
+        res.json({
+            results: usuario ? [usuario] : []
+        })
+    }
+}
 
 
 ///api/buscar/coleccion/termino
@@ -6,10 +29,30 @@ const buscar = (req, res = response) => {
 
     const { coleccion, termino } = req.params;
 
-    res.json({
-        coleccion,
-        termino
-    })
+    if (!coleccionesPermitidas.includes(coleccion)) {
+        res.status(400).json({
+            msg: `Las colecciones permitidas son ${coleccionesPermitidas}`
+        })
+    }
+
+    switch (coleccion) {
+
+
+        case 'usuarios':
+            buscarUsuarios(termino, res);
+            break;
+        case 'categoria':
+            break;
+        case 'productos':
+            break;
+
+        default:
+            res.status(500).json({
+                msg: ' Se le olvido hacer esta busqueda'
+            })
+            break;
+    }
+
 }
 
 
